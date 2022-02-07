4a32fc255fa97f26ac79969d2256d259127ff3c6
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-02-07 18:51:27 +0200

    hw-6-8

--- js/216.js
+++ js/216.js
@@ -26,3 +26,23 @@ inputEl.addEventListener('blur', event => {
 });
 
 age > 18 ? (  alert("Хорошо, вы можете продолжить."),  location.assign("continue.html")) : (  stop = true,  alert("Простите, вы ещё так юны!"));
+
+
+
+const formEl = document.querySelector(".login-form");
+const inputEmail = document.querySelector ('input[name="email"]');
+const inputPassword = document.querySelector ('input[name="password"]');
+const inputButton = document.querySelector ('button');
+
+formEl.addEventListener('submit',(event)=>{inputEmail.value===""||inputPassword.value===""?(alert("Заполните, пожалуйста, поля: Email и Password!"), event.preventDefault()):
+(
+    event.preventDefault();
+    const {elements:{email,password}} = event.currentTarget;
+    console.log(email.value, password.value);
+    formEl.reset();
+)
+}
+)
+
+console.log(formEl, inputEmail, inputPassword,inputButton)
+console.log(elements)
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+const inputEmail = document.querySelector ('input[name="email"]');
+const inputPassword = document.querySelector ('input[name="password"]');
+const inputButton = document.querySelector ('button');
 
+formEl.addEventListener('submit',(event)=>{
+    inputEmail.value===""||inputPassword.value===""?
+    (alert("Заполните, пожалуйста, поля: Email и Password!"), event.preventDefault()):
+
+    event.preventDefault();
+    const {elements:{email,password}} = event.currentTarget;
+    console.log(email.value, password.value);
+    formEl.reset();
+}
+)
+
+console.log(formEl, inputEmail, inputPassword,inputButton)
