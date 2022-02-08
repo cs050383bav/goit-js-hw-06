df02afbea1e38fa6d38b261caa57d858ed22626c
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-02-08 20:15:13 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,18 @@
 const inputEl = document.querySelector('#validation-input')
 
 inputEl.addEventListener('blur', () => {
-  event.currentTarget.value.length == inputEl.dataset.length
+  event.currentTarget.value.length === Number(inputEl.dataset.length)
     ? (inputEl.classList.add('valid'), inputEl.classList.remove('invalid'))
     : (inputEl.classList.add('invalid'), inputEl.classList.remove('valid'))
   console.log(inputEl)
+
+  // console.dir(event.currentTarget.value.length);
+  // console.dir(inputEl.dataset.length);
+  // console.log(typeof event.currentTarget.value.length);
+  // console.log(typeof inputEl.dataset.length);
+  // console.log(typeof Number(inputEl.dataset.length));
+
+
   // inputEl.value = "";
 })
 
-console.log(inputEl)
