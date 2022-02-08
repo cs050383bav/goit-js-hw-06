da16c7cc582e7c461d550ea0de4a3415aaf37b3c
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-02-08 20:39:48 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,20 @@
-const formEl = document.querySelector(".login-form");
-const inputEmail = document.querySelector ('input[name="email"]');
-const inputPassword = document.querySelector ('input[name="password"]');
-const inputButton = document.querySelector ('button');
+const formEl = document.querySelector('.login-form')
+const inputEmail = document.querySelector('input[name="email"]')
+const inputPassword = document.querySelector('input[name="password"]')
+const inputButton = document.querySelector('button')
 
-formEl.addEventListener('submit',(event)=>{
-    if (inputEmail.value===""||inputPassword.value==="")
-    {(alert("Заполните, пожалуйста, поля: Email и Password!"), event.preventDefault())}
-else{
-    event.preventDefault();
-    const {elements:{email,password}} = event.currentTarget;
-    console.log(email.value, password.value);
-    formEl.reset();}
-}
-)
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (inputEmail.value === '' || inputPassword.value === '') {
+    alert('Заполните, пожалуйста, поля: Email и Password!')
+    return
+  } else {
+    const {
+      elements: { email, password },
+    } = event.currentTarget
+    console.log(email.value, password.value)
+    formEl.reset()
+  }
+})
 
-console.log(formEl, inputEmail, inputPassword,inputButton)
+console.log(formEl, inputEmail, inputPassword, inputButton)
